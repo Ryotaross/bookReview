6dddf88cc0026b33b371ee34f215a6395ba18230
Ryotaro Shimizu <ryotaro_625@icloud.com>
2021-09-29 00:02:16 +0900

    20210929 1

--- src/components/Registration.tsx
+++ src/components/Registration.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
-import '../App.css';
+import { useState } from 'react';
+
+const Email=()=>{
+  const [name,setName] = useState('');
+  const [email,setEmail] = useState('');
+  const [password,setPassword] = useState('');
+}
 
 class Registration extends React.Component {
+
   render(){
     return (
       <div className="App">
         <h1>新規登録11</h1>
+        <form>
+    
+        </form>
       </div>
     );
   }
