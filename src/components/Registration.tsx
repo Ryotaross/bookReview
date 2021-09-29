a4cf0f47f15e47a4be3d97fbc58c36102db41dcf
Ryotaro Shimizu <ryotaro_625@icloud.com>
2021-09-29 23:27:42 +0900

    20210929 2

--- src/components/Registration.tsx
+++ src/components/Registration.tsx
@@ -1,24 +1,45 @@
 import React from 'react';
 import { useState } from 'react';
 
-const Email=()=>{
+
+function Registration() {
   const [name,setName] = useState('');
   const [email,setEmail] = useState('');
   const [password,setPassword] = useState('');
-}
-
-class Registration extends React.Component {
-
-  render(){
-    return (
-      <div className="App">
-        <h1>新規登録11</h1>
-        <form>
-    
-        </form>
-      </div>
-    );
+  const [token,setToken] = useState('')
+  const [erros,setErros] = useState([])
+  
+  const handleSubmit = () => {
+    const requestOptions ={
+      method: 'POST',
+      headers:{'accept': 'application/json','Content-Type': 'application/json'},
+      body: JSON.stringify({name: name,email:email,password:password})
+    };
+    fetch("https://api-for-missions-and-railways.herokuapp.com/users",requestOptions)
+      .then((response)=> response.json()
+      )
+      .then((response) =>{
+        console.log("great");
+      })
+      .catch((error)=>{
+        console.log(error);
+      })
+      return false;
   }
+  
+
+  return (
+    <div className="App">
+      <h1>新規登録</h1>
+      <form onSubmit={handleSubmit}>
+        <input value={name} onChange={event => setName(event.target.value)}/>
+        <input value={email} onChange={event => setEmail(event.target.value)}/>
+        <input value={password} onChange={event => {setPassword(event.target.value)}}/>
+        <input type="submit" value="登録"/>
+      </form>
+      <p>{name}</p>
+    </div>
+  );
 }
 
 export default Registration;
